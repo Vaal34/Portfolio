b2ec350b149cfc33e4680c2550fe1f995091cc98
Vaal34 <5619@holbertonstudents.com>
2024-10-14 16:33:49 +0200

    Refactor aboutSection component and update profile image

--- portfolio/src/components/common/content/aboutSection/aPropros.tsx
+++ portfolio/src/components/common/content/aboutSection/aPropros.tsx
@@ -3,17 +3,17 @@ function APropos() {
     <div className="pt-9 text-justify text-background">
       <p className="mb-4 bg-background px-9 py-6 text-primary shadow-lg transition-all hover:scale-[101%]">
         Passionné par le développement logiciel, je suis diplômé de la
-        <strong className="bg-accent">Holberton School</strong>. Avant de
+        <strong className="bg-accent"> Holberton School</strong>. Avant de
         commencer cette aventure, j'ai travaillé comme
-        <strong>opérateur de presse offset</strong>, ce qui m'a permis de
+        <strong> opérateur de presse offset</strong>, ce qui m'a permis de
         développer des compétences en travail d'équipe, gestion de clientèle, et
         en utilisation des outils Adobe comme InDesign et Illustrator. Cette
-        expérience m’a appris la <strong>rigueur et la précision</strong>, des
+        expérience m’a appris la <strong> rigueur et la précision</strong>, des
         qualités que j'applique maintenant dans mon travail de développeur.
       </p>
       <p className="mb-4 border border-background px-9 py-6 text-background transition-all hover:bg-background hover:text-primary">
         Aujourd'hui, mon objectif est de
-        <strong>devenir développeur full-stack sénior</strong>, et j'aime
+        <strong> devenir développeur full-stack sénior</strong>, et j'aime
         relever des défis qui nécessitent à la fois des compétences techniques
         et des capacités de résolution de problèmes.
       </p>
@@ -21,7 +21,7 @@ function APropos() {
       <p className="font-thin italic">
         Je suis toujours à la recherche de nouvelles opportunités de
         collaboration et de projets stimulants. N'hésitez pas à me
-        <a className="cursor-pointer font-normal underline">contacter</a> si
+         <a className="cursor-pointer font-normal"> contacter</a> si
         vous avez un projet ou une opportunité à partager.
       </p>
     </div>
