75346b10d43c6d4fc41163b3fb19160323c4c8aa
Vaal34 <5619@holbertonstudents.com>
2024-10-14 16:36:12 +0200

    Remove unused animation components

--- portfolio/src/animations/hyperText.tsx
+++
@@ -1,8 +0,0 @@
-import HyperText from "@/components/magicui/hyper-text";
-
-export function HyperTextCustom({
-  text,
-  className,
-}: Readonly<{ text: string; className: string }>) {
-  return <HyperText className={className} text={text} />;
-}
