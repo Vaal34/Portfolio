640cbfb1f87ef033d8cd7ecf21d5a76004e45580
Vaal34 <5619@holbertonstudents.com>
2024-10-15 16:19:07 +0200

    Add favicon to HTML head

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <ActiveContentProvider>
         <body className={`${inter.className} hide-scrollbar bg-background`}>
           {children}
